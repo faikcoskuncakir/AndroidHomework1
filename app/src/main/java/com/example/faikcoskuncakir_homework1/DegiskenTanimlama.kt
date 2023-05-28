338dfb69c50fe42e837fa53a2fd02b5113e8677d
Faik Coşkun Çakır <fcoskuncakir@hotmail.com>
2023-05-28 22:47:21 +0300

    first commit
    first commit

---
+++ app/src/main/java/com/example/faikcoskuncakir_homework1/DegiskenTanimlama.kt
@@ -0,0 +1,50 @@
+package com.example.faikcoskuncakir_homework1
+
+fun main() {
+    val sehir = "Trabzon"
+    val ulke = "Türkiye"
+    val telefon = "(507) 314 43 40"
+    val postaKodu = 34250
+    val eMail = "fcoskuncakir@hotmail.com"
+    val meslek = "Developer"
+    val stokMiktari = 61
+    val musteriAdı = "Yusuf Avcı"
+    val bakiye = 61.000
+    val dogumGunu = "20.10.1995"
+    val maas = 25.000
+    val medeniDurumu = "Evli"
+    val urunYorumu = "Olumlu"
+    val odemeTarihi = "01.01.2025"
+    val odeme = "Yapıldı"
+    val siparisAdeti = 55
+    val arabaModeli = "Karoq"
+    val kitapAdi = "Ruh Adam"
+    val yayinlanmaTarihi = "25.04.1980"
+    val indirimMiktari = "%50"
+    val odaSayisi = 5
+    val enlem = 25.40
+    val boylam = 44.62
+    val urunAdi = "Telefon"
+    val yemekFiyati = "190 TL"
+    val marka = "Skoda"
+    val muzikAdi = "Gözleri Fettan Güzel"
+    val videoSuresi = "3.42 Dk"
+    val urunPuani = 4.53
+    val resimAdı = "Monalisa"
+    val dosyaFormati = ".pptx"
+    val renk = "Mor"
+    val renkKodu = "MASFLASD"
+    val telefonModeli = "iPhone 14"
+    val ekranBoyutu = "16.4"
+    val agirlik = 80
+    val ulusalGun = "29.10.1923"
+    val tatilGunu = "Pazar"
+    val rezervasyonTarihi = "01.01.2024"
+    val sokakAdi = "Cumhuriyet"
+    val otobusHatti = "36M"
+    val kalanDakika = 3
+    val takipKodu = 23452834829523
+    val kuponSuresi = "3 Hafta"
+    val kuponKodu = "SDFA7876ASF7"
+    val faturaAdresi = "Gaziosmanpaşa-İstanbul-34250"
+}
